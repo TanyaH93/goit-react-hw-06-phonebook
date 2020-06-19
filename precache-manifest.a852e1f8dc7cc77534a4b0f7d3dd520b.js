76ce455cc2972edd92c977f9914db58fa27163fb
Tanya <fullstackon@gmail.com>
2020-06-19 19:53:44 +0300

    Updates

---
+++ precache-manifest.a852e1f8dc7cc77534a4b0f7d3dd520b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa5c522fe9de5a318d3f84237fba85e",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "af55bb431b28f12f6426",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bbe3a416f2fe3a884fbb",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.016c342b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.016c342b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af55bb431b28f12f6426",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.e996548a.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goit-react-hw-06-phonebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
